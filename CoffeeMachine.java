9899dbdd5ea5d7cec4844715b4f723c721712d9a
Yael Lozano <68799691+MrYaello@users.noreply.github.com>
2020-08-06 01:45:34 -0500

    Version Updated
    Includes multilanguage - es , en -
    Execute a single JAR file!

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,3 +1,7 @@
+package machine;
+
+import static machine.lang.*;
+import static machine.Main.*;
 import java.util.Scanner;
 
 public class CoffeeMachine {
@@ -9,12 +13,12 @@ public class CoffeeMachine {
     public static int money = 550;
 
     public static void remaining() {
-        print("The coffee machine has:");
-        print(water + " of water");
-        print(milk + " of milk");
-        print(beans + " of coffee beans");
-        print(cups + " of disposable cups");
-        print(money + " of money");
+        print(machineHas);
+        print(water + ofWater);
+        print(milk + ofMilk);
+        print(beans + ofBeans);
+        print(cups + ofCups);
+        print(money + ofMoney);
         print("");
     }
 
@@ -22,154 +26,202 @@ public class CoffeeMachine {
         System.out.println(print);
     }
 
-    public static void menu() {
-        Scanner sc = new Scanner(System.in);
-
-        print("Write action (buy, fill, take, remaining, exit):");
-        String menu = sc.next().toLowerCase();
-
-        switch (menu) {
-            case "buy":
-                print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
-                String coffee = sc.next().toLowerCase();
-                switch (coffee) {
-                    case "1": //espresso
-                        if (water >= 250) {
-                            if (beans >= 16) {
-                                if (cups >= 1) {
-                                    water -= 250;
-                                    beans -= 16;
-                                    cups -= 1;
-                                    money += 4;
-                                    print("I have enough resources, making you a coffee!");
-                                } else {
-                                    print("Sorry, not enough disposable cups!");
-                                }
-                            } else {
-                                print("Sorry, not enough coffee beans!");
-                            }
+    public static void buy() {
+        Scanner scanner = new Scanner(System.in);
+        print(wantBuy);
+        String coffee = scanner.next().toLowerCase();
+        switch (coffee) {
+            case "1": //espresso
+                if (water >= 250) {
+                    if (beans >= 16) {
+                        if (cups >= 1) {
+                            water -= 250;
+                            beans -= 16;
+                            cups -= 1;
+                            money += 4;
+                            print(making);
                         } else {
-                            print("Sorry, not enough water!");
+                            print(notCups);
                         }
-                        print("");
-                        menu();
-                        break;
-
-                    case "2": //latte
-                        if (water >= 350) {
-                            if (milk >= 75) {
-                                if (beans >= 20) {
-                                    if (cups >= 1) {
-                                        water -= 350;
-                                        milk -= 75;
-                                        beans -= 20;
-                                        cups -= 1;
-                                        money += 7;
-                                        print("I have enough resources, making you a coffee!");
-                                    } else {
-                                        print("Sorry, not enough disposable cups!");
-                                    }
-                                } else {
-                                    print("Sorry, not enough coffee beans!");
-                                }
-                            } else {
-                                print("Sorry, not enough milk!");
-                            }
-                        } else {
-                            print("Sorry, not enough water!");
-                        }
-                        print("");
-                        menu();
-                        break;
-
-                    case "3": // cappuccino
-                        if (water >= 200) {
-                            if (milk >= 100) {
-                                if (beans >= 12) {
-                                    if (cups >= 1) {
-                                        water -= 200;
-                                        milk -= 100;
-                                        beans -= 12;
-                                        cups -= 1;
-                                        money += 6;
-                                        print("I have enough resources, making you a coffee!");
-                                    } else {
-                                        print("Sorry, not enough disposable cups!");
-                                    }
-                                } else {
-                                    print("Sorry, not enough coffee beans!");
-                                }
-                            } else {
-                                print("Sorry, not enough milk!");
-                            }
-                        } else {
-                            print("Sorry, not enough water!");
-                        }
-                        print("");
-                        menu();
-                        break;
-
-                    case "back":
-                        print("");
-                        menu();
-                        break;
-
-                    default:
-                        print("Invalid action!");
-                        print("");
-                        menu();
-                        break;
+                    } else {
+                        print(notBeans);
+                    }
+                } else {
+                    print(notWater);
                 }
-                break;
-
-            case "fill":
-                print("Write how many ml of water do you want to add:");
-                int addWater = sc.nextInt();
-                water += addWater;
-
-                print("Write how many ml of milk do you want to add:");
-                int addMilk = sc.nextInt();
-                milk += addMilk;
-
-                print("Write how many grams of coffee beans do you want to add:");
-                int addBeans = sc.nextInt();
-                beans += addBeans;
-
-                print("Write how many disposable cups of coffee do you want to add:");
-                int addCups = sc.nextInt();
-                cups += addCups;
-
                 print("");
-                menu();
                 break;
 
-            case "take":
-                print("I gave you $" + money);
-                money = 0;
-
+            case "2": //latte
+                if (water >= 350) {
+                    if (milk >= 75) {
+                        if (beans >= 20) {
+                            if (cups >= 1) {
+                                water -= 350;
+                                milk -= 75;
+                                beans -= 20;
+                                cups -= 1;
+                                money += 7;
+                                print(making);
+                            } else {
+                                print(notCups);
+                            }
+                        } else {
+                            print(notBeans);
+                        }
+                    } else {
+                        print(notMilk);
+                    }
+                } else {
+                    print(notWater);
+                }
                 print("");
-                menu();
                 break;
 
-            case "remaining":
-                remaining();
-
-                menu();
+            case "3": // cappuccino
+                if (water >= 200) {
+                    if (milk >= 100) {
+                        if (beans >= 12) {
+                            if (cups >= 1) {
+                                water -= 200;
+                                milk -= 100;
+                                beans -= 12;
+                                cups -= 1;
+                                money += 6;
+                                print(making);
+                            } else {
+                                print(notCups);
+                            }
+                        } else {
+                            print(notBeans);
+                        }
+                    } else {
+                        print(notMilk);
+                    }
+                } else {
+                    print(notWater);
+                }
+                print("");
                 break;
 
-            case "exit":
+            case "back":
+                print("");
                 break;
 
             default:
-                System.out.println("Invalid action!");
-
+                print(invalid);
                 print("");
-                menu();
                 break;
         }
+
     }
 
-    public static void main(String[] args) {
-        menu();
+    public static void fill() {
+        Scanner scan = new Scanner(System.in);
+        print(manyWater);
+        int addWater = scan.nextInt();
+        water += addWater;
+
+        print(manyMilk);
+        int addMilk = scan.nextInt();
+        milk += addMilk;
+
+        print(manyBeans);
+        int addBeans = scan.nextInt();
+        beans += addBeans;
+
+        print(manyCups);
+        int addCups = scan.nextInt();
+        cups += addCups;
+
+        print("");
+    }
+
+    public static void take() {
+        print(take + money);
+        money = 0;
+
+        print("");
+    }
+
+    public static void menu() {
+        Scanner sc = new Scanner(System.in);
+
+        print(action);
+        String menu = sc.next().toLowerCase();
+
+        if ("en".equals(lang)) {
+            switch (menu) {
+                case "buy":
+                    buy();
+
+                    menu();
+                    break;
+
+                case "fill":
+                    fill();
+
+                    menu();
+                    break;
+
+                case "take":
+                    take();
+
+                    menu();
+                    break;
+
+                case "remaining":
+                    remaining();
+
+                    menu();
+                    break;
+
+                case "exit":
+                    break;
+
+                default:
+                    print(invalid);
+
+                    print("");
+                    menu();
+                    break;
+            }
+        } else if ("es".equals(lang)) {
+            switch (menu) {
+                case "comprar":
+                    buy();
+
+                    menu();
+                    break;
+
+                case "llenar":
+                    fill();
+
+                    menu();
+                    break;
+
+                case "retirar":
+                    take();
+
+                    menu();
+                    break;
+
+                case "restante":
+                    remaining();
+
+                    menu();
+                    break;
+
+                case "salir":
+                    break;
+
+                default:
+                    print(invalid);
+
+                    print("");
+                    menu();
+                    break;
+            }
+        }
     }
 }
